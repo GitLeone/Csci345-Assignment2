d8cf127c601fe5a059fbb835286dac393320858c
Jerico Rosier-Butler <jericoleonerb@gmail.com>
2025-05-05 18:01:13 -0700

    Added SceneCard

---
+++ SceneCard.java
@@ -0,0 +1,30 @@
+public class SceneCard{
+    private String name;
+    private int budget;
+    private boolean flipped;
+    
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getBudget(){
+        return this.budget;
+    }
+    public void setBudget(int budget){
+        this.budget = budget;
+    }
+    public boolean getFlipped(){
+        return this.flipped;
+    }
+    public void setFlipped(boolean flipped){
+        this.flipped = flipped;
+    }
+
+    public void wrapScene(){
+
+    }
+
+}
