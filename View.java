741e2ceeca506e936d82984529a6254183206cf0
Joshua Pendlelton <joshuapendd@gmail.com>
2025-05-13 17:42:44 -0700

    View work

--- View.java
+++ View.java
@@ -1,3 +1,6 @@
 public interface View {
     void displayMessage(String message);
+    void displayPlayerStatus(Player player);
+    void displayLocation(Set set);
+    void displayActResult(boolean success, int reward, String rewardType);
 }
