1d6a32c097ba249468614e2b524dc116a43a966e
Jerico Rosier-Butler <jericoleonerb@gmail.com>
2025-05-28 13:20:45 -0700

    added GUIController

---
+++ GUIController.java
@@ -0,0 +1,4 @@
+//This is the GUI implementation of GameController
+public class GUIController{
+    
+}
--- GUIView.java
+++ GUIView.java
@@ -1,3 +1,5 @@
+//Will have functions from View just like TextView
+//This is the GUI implementation of TextView
 public class GUIView implements View{
     private GameController moderator;
     private LocationManager lm;
