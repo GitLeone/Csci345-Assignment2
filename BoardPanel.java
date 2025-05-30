b331eeecdd6cdb43ed2d18861f37f4c054d28101
Jerico Rosier-Butler <jericoleonerb@gmail.com>
2025-05-30 14:06:43 -0700

    cleaned up images

--- BoardPanel.java
+++ BoardPanel.java
@@ -3,10 +3,16 @@ import javax.swing.*;
 
 public class BoardPanel extends JPanel {
     private final GUIController model;
+    private JLabel boardLabel;
     
     public BoardPanel(GUIController model) {
         this.model = model;
-        setPreferredSize(new Dimension(800, 600));
         setLayout(null);
+        ImageIcon board = new ImageIcon("images/board.jpg");
+        boardLabel = new JLabel(board);
+        boardLabel.setBounds(0, 0, board.getIconWidth(), board.getIconHeight());
+
+        add(boardLabel);
+        setPreferredSize(new Dimension(board.getIconWidth(), board.getIconHeight()));
     }
 }
