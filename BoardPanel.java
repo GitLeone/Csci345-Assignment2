25855ccb696e894d058e8b04611056e1a4bdccae
Jerico Rosier-Butler <jericoleonerb@gmail.com>
2025-05-31 00:11:32 -0700

    working on cardBack

--- BoardPanel.java
+++ BoardPanel.java
@@ -5,12 +5,16 @@ import javax.swing.*;
 
 public class BoardPanel extends JPanel {
     private GameController gameController;
+    private LocationManager locationManager;
     private Image boardImage;
+    private Image cardBackImage;
     private Map<String, Point> locationCoordinates;
 
     public BoardPanel(GameController controller) {
         this.gameController = controller;
+        this.locationManager = controller.getLocationManager();
         loadBoardImage();
+        loadFaceDownCards();
         initializeLocationCoordinates();
         setPreferredSize(new Dimension(boardImage.getWidth(this), boardImage.getHeight(this)));
     }
@@ -20,18 +24,33 @@ public class BoardPanel extends JPanel {
         boardImage = icon.getImage();
     }
 
+    private void loadFaceDownCards() {
+        ImageIcon icon = new ImageIcon("images/Card/01.png");
+        cardBackImage = icon.getImage();
+    }
+
     private void initializeLocationCoordinates() {
         locationCoordinates = new HashMap<>();
         // Initialize with coordinates from your XML or hardcoded
-        locationCoordinates.put("trailer", new Point(100, 100));
-        locationCoordinates.put("office", new Point(200, 200));
-        // Add all other locations...
+        for(Set value : locationManager.getSetList().values()){
+            locationCoordinates.put(value.getName(), new Point(value.getXCord(), value.getYCord()));
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(boardImage, 0, 0, this);
+
+        //Draws the face down cards
+        for (Set set : locationManager.getSetList().values()) {
+            if (set.isSet()) {
+                Point pos = locationCoordinates.get(set.getName());
+                if (pos != null) {
+                    g.drawImage(cardBackImage, pos.x, pos.y, this);
+                }
+            }
+        }
         
         // Draw players
         for (Player player : gameController.getPlayers()) {
