7c8c1dd2488d3fd68a145e430dad391e1aff5b27
Jerico Rosier-Butler <jericoleonerb@gmail.com>
2025-06-04 17:57:25 -0700

    finished setting base points

--- LocationManager.java
+++ LocationManager.java
@@ -110,6 +110,96 @@ public class LocationManager{
                 set.addBasePoint(new Point(782, 401));
                 set.addBasePoint(new Point(822, 401));  
             }
+            else if(name.equals("hotel")){
+                set.addBasePoint(new Point(1000, 470));
+                set.addBasePoint(new Point(1000, 510));
+                set.addBasePoint(new Point(1000, 550));
+                set.addBasePoint(new Point(1000, 590)); 
+                set.addBasePoint(new Point(946, 856));
+                set.addBasePoint(new Point(986, 856));
+                set.addBasePoint(new Point(1121, 856));
+                set.addBasePoint(new Point(1161, 856));
+            }
+            else if(name.equals("bank")){
+                set.addBasePoint(new Point(610, 593));
+                set.addBasePoint(new Point(650, 593));
+                set.addBasePoint(new Point(765, 593));
+                set.addBasePoint(new Point(805, 593)); 
+                set.addBasePoint(new Point(845, 593));
+                set.addBasePoint(new Point(835, 463));
+                set.addBasePoint(new Point(835, 503));
+                set.addBasePoint(new Point(875, 463));
+            }
+            else if(name.equals("church")){
+                set.addBasePoint(new Point(609, 852));
+                set.addBasePoint(new Point(649, 852));
+                set.addBasePoint(new Point(773, 852));
+                set.addBasePoint(new Point(813, 852));
+                set.addBasePoint(new Point(737, 688));
+                set.addBasePoint(new Point(777, 688));
+                set.addBasePoint(new Point(817, 688));
+                set.addBasePoint(new Point(857, 688));
+            }
+            else if(name.equals("secret hideout")){
+                set.addBasePoint(new Point(240, 845));
+                set.addBasePoint(new Point(280, 845));
+                set.addBasePoint(new Point(320, 845));
+                set.addBasePoint(new Point(360, 845));
+                set.addBasePoint(new Point(240, 805));
+                set.addBasePoint(new Point(280, 805));
+                set.addBasePoint(new Point(320, 805));
+                set.addBasePoint(new Point(360, 805));
+            }
+            else if(name.equals("ranch")){
+                set.addBasePoint(new Point(267, 652));
+                set.addBasePoint(new Point(307, 652));
+                set.addBasePoint(new Point(347, 647));
+                set.addBasePoint(new Point(541, 600));
+                set.addBasePoint(new Point(541, 560));
+                set.addBasePoint(new Point(541, 520));
+                set.addBasePoint(new Point(267, 612));
+                set.addBasePoint(new Point(307, 612));
+            }
+            else if(name.equals("office")){
+                set.addBasePoint(new Point(5, 495));
+                set.addBasePoint(new Point(5, 535));
+                set.addBasePoint(new Point(5, 575));
+                set.addBasePoint(new Point(5, 605));
+                set.addBasePoint(new Point(175, 495));
+                set.addBasePoint(new Point(175, 535));
+                set.addBasePoint(new Point(175, 575));
+                set.addBasePoint(new Point(175, 605));
+            }
+            else if(name.equals("train station")){
+                set.addBasePoint(new Point(10, 217));
+                set.addBasePoint(new Point(10, 257));
+                set.addBasePoint(new Point(10, 297));
+                set.addBasePoint(new Point(10, 337));
+                set.addBasePoint(new Point(50, 217));
+                set.addBasePoint(new Point(118, 391));
+                set.addBasePoint(new Point(-4, 16));
+                set.addBasePoint(new Point(195, 16));
+            }
+            else if(name.equals("jail")){
+                set.addBasePoint(new Point(513, 199));
+                set.addBasePoint(new Point(473, 199));
+                set.addBasePoint(new Point(433, 199));
+                set.addBasePoint(new Point(393, 199));
+                set.addBasePoint(new Point(353, 199));
+                set.addBasePoint(new Point(313, 199));
+                set.addBasePoint(new Point(400, 148));
+                set.addBasePoint(new Point(313, 159));
+            }
+            else if(name.equals("general store")){
+                set.addBasePoint(new Point(197, 400));
+                set.addBasePoint(new Point(197, 360));
+                set.addBasePoint(new Point(197, 320));
+                set.addBasePoint(new Point(197, 280));
+                set.addBasePoint(new Point(280, 400));
+                set.addBasePoint(new Point(280, 360));
+                set.addBasePoint(new Point(280, 320));
+                set.addBasePoint(new Point(280, 280));
+            }
         }
     }
 }
